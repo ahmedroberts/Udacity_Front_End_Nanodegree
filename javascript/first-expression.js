8b636d3d314609904a4d63ffa8c52c3e81d444ab
Ahmed Roberts <52471253+ahmedroberts@users.noreply.github.com>
2024-05-11 22:22:20 -0700

    add first-expression.js

---
+++ javascript/first-expression.js
@@ -0,0 +1,33 @@
+/*
+ * Programming Quiz: First Expression
+ * Write an expression that uses at least three, distinct, arithmetic 
+ * operators to log the number 42 to the console.
+ */
+ 
+/*
+ * QUIZ REQUIREMENTS
+ * 1. Your code should print the value `42`
+ * 2. You should use at least 3 distinct operators.
+ *    (`+`, `-`, `*`, `/`, or `%`) 
+ */
+
+
+// this expression equals 4, change it to equal 42
+console.log(1 + 5 - 2);
+console.log(1 + 5 - 2 + 10 - (12 / 3) + (6 ** 2) - (19 % 5));
+// Raikage expects 42
+
+/* Type Error
+const dinner = "tacos";
+dinner = dinner + " and " + "tamales";
+console.log(dinner);
+
+let dinner = "tacos";
+dinner = dinner + " and " + "tamales";
+console.log(dinner);
+
+Syntax Error
+let dinner2 = "tacos";
+const dinner2 =  "tamales";
+console.log(dinner2);
+*/
