ab9d802a4085c49ce7091ebc2180ec067bb04db3
Ahmed Roberts <52471253+ahmedroberts@users.noreply.github.com>
2024-05-18 22:42:30 -0700

    add boolean and quick `if`` statements

--- javascript/favorite-food.js
+++ javascript/favorite-food.js
@@ -9,3 +9,23 @@
  * 3. Your code must have a `console.log()` function
  */
 console.log(/* replace this comment with your string */);
+
+// Ahmed Code -- 2025-05-16
+console.log('Ribs');
+
+const studentName = "Naruto";
+const haveEnrolledInCourse = true;
+const haveCompletedTheCourse = false;
+
+if (haveEnrolledInCourse) {
+  console.log("Welcome "+studentName+" to Shinobi School");
+}
+
+const aa = 10;
+const bb = 20;
+
+if (aa > bb) {
+  console.log("Variable `aa` has a higher value");
+} else {
+  console.log("Variable `bb` is greater than or equal to `aa`");
+}
---
+++ javascript/sienna.js
@@ -0,0 +1,15 @@
+// Ahmed O. Roberts
+// 2023-02-03
+// 7 months before the big day
+
+console.log('Jumbo Rafiki');
+
+const ahmed_raikage_message = 'Ahmed is the 9th Raikage!! \n\n\t R41K4G3';
+// 2024-05-16 - Back studying JavaScript
+// Wife named it `Sienna`
+console.log(ahmed_raikage_message)
+
+const body = document.getElementsByTagName("body")[0];
+body.style.backgroundColor = "#FF00FF";
+body.style.padding = "5rem";
+body.style.color = "#FFFFF8";
