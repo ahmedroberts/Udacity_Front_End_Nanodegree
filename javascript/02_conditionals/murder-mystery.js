d308eb25e1e4ff772e498db3f962d27a7362d0ba
Ahmed Roberts <52471253+ahmedroberts@users.noreply.github.com>
2024-05-20 12:48:55 -0700

    Update murder mystery solve

--- javascript/02_conditionals/murder-mystery.js
+++ javascript/02_conditionals/murder-mystery.js
@@ -15,7 +15,7 @@
 
 
 /* ****************************************** */
-/* TESTING LOGIC */ 
+/* TESTING LOGIC 
 // Change the value of `room` and `suspect` to test your code
 
 const room = "billiards room";
@@ -34,19 +34,35 @@ let solved = false;
 * 1. sets the value of weapon based on the room and
 * 2. sets the value of solved to true if the value of room matches the suspect's room
 */
-if (/* your conditional goes here */) {
-    
-} else if (/* your conditional goes here */) {
-    
-} else if (/* your conditional goes here */) {
-    
-} else {
-    
+
+/* const room = "billiards room";
+ * const suspect = "Mr. Parkes"; 
+const room = "dining room";
+const suspect = "Mr. Parkes"; */
+const room = "gallery";
+const suspect = "Ms. Van Cleve";
+
+if (room == "ballroom" && suspect == "Mr. Kalehoff") {
+  weapon = "poison";
+  solved = true;
+  
+} else if (room == "billiards room" && suspect == "Mrs. Sparr") {
+  weapon = "pool stick";
+  solved = true;
+  
+} else if (room == "dining room" && suspect == "Mr. Parkes") {
+  weapon = "knife";
+  solved = true;
+  
+} else if (room == "gallery" && suspect == "Ms. Van Cleve") {
+  weapon = "trophy";
+  solved = true;
+
 }
 /* ****************************************** */
 // The code below will run only when `solved` is true
 if (solved) {
-	console.log(/* your message goes here*/);
+	console.log(suspect + " did it in the " + room + " with the " + weapon + "!");
 } else {
   console.log("The case is not solved!")
 }
