0b9753a8b68a2d6f460fbd3587614a11a200a31c
Ahmed Roberts <52471253+ahmedroberts@users.noreply.github.com>
2024-05-29 22:53:47 -0700

    Study: `For` loops in js

--- javascript/03_loops/loops-practice.js
+++ javascript/03_loops/loops-practice.js
@@ -2,5 +2,24 @@
 Ahmed The 9th Raikage 
 Loops in JavaScript
 */
+
+/* While Loops */
 // let x = 1; while (x <= 10000) { console.log(x + " mississippi!"); x = x + 1; }
 let x = 1; while (x <= 9) { console.log(x + " mississippi!"); x = x + 1; } // loop to 9
+
+/* For Loops
+for ( start; stop; step ) {               //  Start - Stop - Step  <<<<<
+  // do this thing
+}
+*/
+
+for (let i = 0; i < 6; i = i + 1) {
+  console.log("Printing out i = " + i);
+}
+
+// Nested Loops
+for (let x = 0; x < 3; x = x + 1) {
+  for (let y = 0; y < 2; y = y + 1) {
+      console.log(x + ", " + y);
+  }
+}
